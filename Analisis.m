4ac92327bf782d4e5e4f29da150105c80207be6a
Jhon Vargas <jhevargasv@correo.udistrital.edu.co>
2021-02-03 08:22:03 -0500

    Primeros pasos del analisis

--- Analisis.m
+++ Analisis.m
@@ -1,3 +1,20 @@
 clc; clear all; close all;
 
-data = load('Data\data_2\exp1.mat','mejor_FIS_validacion');
+experimento = 2;
+
+for q=1:1:15
+    fprintf('Cargando los datos del experimento %d \n',q);
+    data(q) = load(['Data\data_',num2str(experimento),'\exp',num2str(q),'.mat'],'semax','vs0','vsf');
+end 
+ 
+ for q=17:1:21
+    fprintf('Cargando los datos del experimento %d \n',q);
+    data(q) = load(['Data\data_',num2str(experimento),'\exp',num2str(q),'.mat'],'semax','vs0','vsf');
+ end 
+ 
+ 
+ %data_min =cell2mat (struct2cell(data));
+ 
+ %semax=min(data); 
+ 
+ %semax=max(max(curvas_error)); % máximo error de validación S
