847f1b58c010791a53b3851e7d22c68b0c05edf2
Jhon Vargas <jhevargasv@correo.udistrital.edu.co>
2021-02-08 11:58:53 -0500

    analisis con falta del minimo

--- Analisis.m
+++ Analisis.m
@@ -1,3 +1,6 @@
+%% Analisis Proyecto #2
+% JEV 20152005077
+% Carga de variables individuales
 clc; clear all; close all;
 
 experimento = 4;
@@ -11,10 +14,9 @@ experimento = 4;
      semax(q,:) = load(['Data\data_',num2str(experimento),'\exp',num2str(q),'.mat'],'semax');
      Vs0(q,:) = load(['Data\data_',num2str(experimento),'\exp',num2str(q),'.mat'],'vs0');
      Vsf(q,:) = load(['Data\data_',num2str(experimento),'\exp',num2str(q),'.mat'],'vsf');     
-      % curvas_error_entre(i,:) = trainError;
  end 
-  
-  for q=17:1:21
+ 
+ for q=17:1:21
      fprintf('Cargando los datos del experimento %d \n',q);
      reglas(q,:) = load(['Data\data_',num2str(experimento),'\exp',num2str(q),'.mat'],'M');
      Nit(q,:) = load(['Data\data_',num2str(experimento),'\exp',num2str(q),'.mat'],'epoch');
@@ -25,6 +27,7 @@ experimento = 4;
      Vsf(q,:) = load(['Data\data_',num2str(experimento),'\exp',num2str(q),'.mat'],'vsf');     
   end 
 
+
 reglas = cell2mat( struct2cell( reglas) );
 semax  = cell2mat( struct2cell( semax) ) ;
 Nit    = cell2mat( struct2cell( Nit) )   ;
@@ -33,12 +36,35 @@ nrun   = cell2mat( struct2cell( nrun) )  ;
 Vs0    = cell2mat( struct2cell( Vs0) )   ;
 Vsf    = cell2mat( struct2cell( Vsf) )   ;
 
-
 save(['data_',num2str(experimento),'_exp'],...
-    'reglas','semax','Nit','tasa','nrun','Vs0','Vsf');
+     'reglas','semax','Nit','tasa','nrun','Vs0','Vsf');
+%% Carga de strings
 
-%%
-min
+clc; clear all; close all;
 
-%semax=min(data); 
-%semax=max(max(curvas_error)); % máximo error de validación S
+experimento = 3;
+tic
+for q=1:1:15 
+    fprintf('Cargando los datos del experimento %d \n',q);
+    load(['Data\data_',num2str(experimento),'\exp',num2str(q),'.mat']);
+
+    trainErro = min(min(curvas_error_entre));
+    chkErro = min(min(curvas_error));
+
+    trainError(q,:) = trainErro;
+    chkError(q,:) = chkErro;
+end
+
+for q=17:1:21 
+    fprintf('Cargando los datos del experimento %d \n',q);
+    load(['Data\data_',num2str(experimento),'\exp',num2str(q),'.mat']);
+
+    trainErro = min(min(curvas_error_entre));
+    chkErro = min(min(curvas_error));
+
+    trainError(q,:) = trainErro;
+    chkError(q,:) = chkErro;
+end
+toc
+save(['data_numero_',num2str(experimento),'_exp'],...
+    'trainError','chkError');
